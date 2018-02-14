ecb846c03fe9514a9071371c159c0290e7321dcb
jnsone11 <jens.neuse@gmx.de>
2018-02-14 12:47:30 +0100

    update proto

---
+++ README.md
@@ -0,0 +1,20 @@
+# Postgres Logical Replication to Golang
+
+## Why?
+
+``
+Keep applications in sync with a source of truth (postgresql datbase).
+``
+
+## How?
+
+``
+With the use of postgres logical replication and the debezium decoderbufs plugin we're able to replicate database changes to a golang application via protocol buffers.
+``
+
+## Facts
+
+- type safe
+- consistent replication
+- high performance
+- low memory footprint
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/jackc/pgx"
 	"log"
 	"context"
+	"github.com/jnsone11/PostgresLogicalReplicationTest/proto"
+	"github.com/gogo/protobuf/proto"
 )
 
 func main() {
@@ -32,7 +34,7 @@ func main() {
 
 	var cp string
 	var snapshot_name string
-	cp, snapshot_name, err = replicationConn.CreateReplicationSlotEx("pgx_test", "test_decoding")
+	cp, snapshot_name, err = replicationConn.CreateReplicationSlotEx("pgx_test", "decoderbufs")
 	if err != nil {
 		log.Fatalf("replication slot create failed: %v", err)
 	}
@@ -57,11 +59,43 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if message.WalMessage != nil {
-			log.Printf("Message:\n%s", string(message.WalMessage.WalData))
+		if message != nil && message.WalMessage != nil {
+
+			var msg decoderbufs.RowMessage
+			err = proto.Unmarshal(message.WalMessage.WalData, &msg)
+			if err != nil {
+				log.Println(err.Error())
+			} else {
+
+				log.Println("--- MSG ---")
+
+				log.Printf("Table: %s", msg.GetTable())
+				log.Printf("Operation: %s", msg.Op.String())
+
+				for _, field := range msg.NewTuple {
+					log.Println("--- FIELD ---")
+
+					log.Printf("Field str: %s", field.String())
+
+					log.Printf("Field ColumnName: %s", field.GetColumnName())
+					log.Printf("Field ColumnType: %d", field.GetColumnType())
+
+					switch field.GetColumnType() {
+					case int64(23):
+						log.Printf("Field Int32: %d", field.GetDatumInt32())
+					case int64(25):
+					case int64(3802):
+						log.Printf("Field String: %s", field.GetDatumString())
+					}
+
+					log.Println("--- FIELD_END ---")
+				}
+
+				log.Println("--- END ---")
+			}
 		}
 
-		if message.ServerHeartbeat != nil {
+		if message != nil && message.ServerHeartbeat != nil {
 			log.Printf("heartbeat: %s", message.ServerHeartbeat.String())
 		}
 	}
